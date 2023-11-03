509b80a94fcd6613e3ddb248e99caf882f77db89
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-11-03 22:54:57 +0200

    part 4

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 export const Button = styled.button`
   border-radius: 8px;
   background-color: #fff;
-  margin: 16px 8px;
   height: 28px;
   font-size: 16px;
   cursor: pointer;
+  text-transform: capitalize;
 `;
 
 export const List = styled.ul`
   display: flex;
+  gap: 12px;
 `;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,10 @@
+import { SectionSty, Title } from './Sections.styled';
+
 export const Section = ({ title, children }) => {
   return (
-    <section>
-      <h2>{title}</h2>
+    <SectionSty>
+      <Title>{title}</Title>
       {children}
-    </section>
+    </SectionSty>
   );
 };
