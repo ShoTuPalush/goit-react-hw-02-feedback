82a5dab9d4f2ced52927cba53a6da858e80e8b7a
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-11-03 20:51:47 +0200

    first part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Button } from './App.styled';
+import { GlobalStyled } from './GlobalStyled';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  onNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+
+  onBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <GlobalStyled />
+        <h2>Please leave feedback</h2>
+        <div>
+          <Button type="button" onClick={this.onGood}>
+            Good
+          </Button>
+          <Button type="button" onClick={this.onNeutral}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={this.onBad}>
+            Bad
+          </Button>
+        </div>
+        <h2 style={{ marginBottom: 20 }}>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </>
+    );
+  }
+}
---
+++ src/components/App.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  border-radius: 8px;
+  background-color: #fff;
+  margin: 16px 8px;
+  height: 28px;
+  font-size: 16px;
+  cursor: pointer;
+`;
